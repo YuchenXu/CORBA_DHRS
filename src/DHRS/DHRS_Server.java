4023b54204fa8f93372e2d1ca8103751c9907541
Yuchen <xycstar90@hotmail.com>
2015-11-30 16:09:11 -0500

    First commit with DHRS CORBA

---
+++ src/DHRS/DHRS_Server.java
@@ -0,0 +1,58 @@
+package DHRS;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
+import org.omg.PortableServer.POAPackage.ObjectNotActive;
+import org.omg.PortableServer.POAPackage.ServantAlreadyActive;
+import org.omg.PortableServer.POAPackage.WrongPolicy;
+
+public class DHRS_Server {
+	/**
+	 * @param args
+	 * @throws WrongPolicy 
+	 * @throws ServantAlreadyActive 
+	 * @throws ObjectNotActive 
+	 * @throws FileNotFoundException 
+	 * @throws AdapterInactive 
+	 */
+	public static void main(String[] args) throws InvalidName, ServantAlreadyActive, WrongPolicy, ObjectNotActive, FileNotFoundException, AdapterInactive {
+
+		ORB orb=ORB.init(args,null);
+		POA rootpoa=POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+		Function_implement H1=new Function_implement(5,4,3);
+		Function_implement H2=new Function_implement(5,4,3);
+		Function_implement H3=new Function_implement(5,4,3);
+		byte[] id1=rootpoa.activate_object(H1);
+		byte[] id2=rootpoa.activate_object(H2);
+		byte[] id3=rootpoa.activate_object(H3);
+		org.omg.CORBA.Object ref1=rootpoa.id_to_reference(id1);
+		org.omg.CORBA.Object ref2=rootpoa.id_to_reference(id2);
+		org.omg.CORBA.Object ref3=rootpoa.id_to_reference(id3);
+		String ior1=orb.object_to_string(ref1);
+		String ior2=orb.object_to_string(ref2);
+		String ior3=orb.object_to_string(ref3);
+		System.out.println(ref1);System.out.println(ref2);System.out.println(ref3);
+		PrintWriter file1=new PrintWriter("ior1.txt");
+		PrintWriter file2=new PrintWriter("ior2.txt");
+		PrintWriter file3=new PrintWriter("ior3.txt");
+		file1.println(ior1);
+		file1.close();
+		file2.println(ior2);
+		file2.close();
+		file3.println(ior3);
+		file3.close();
+		rootpoa.the_POAManager().activate();
+		orb.run();
+
+
+	}
+
+}
---
+++ src/DHRS/Function_implement.java
@@ -0,0 +1,156 @@
+package DHRS;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+public class Function_implement extends functionPOA {
+	HashMap<Integer, Room> reservationinfo=new HashMap<Integer, Room>();
+	ArrayList<Room> Roominfo = new ArrayList<Room>();
+	HashMap<Integer, Guest> reservationcheck=new HashMap<Integer, Guest>();
+	int ReservavtionID=0;
+	String hroominfo="";
+	String serviceReport="";
+	String printstaue="";
+	String transferinfo="";
+	public Function_implement(int a,int b,int c){
+		for(int i=0;i<a;i++){
+			Roominfo.add(new Room(a,"Single",false,0,0,null));
+		}
+		for(int i=0;i<b;i++){
+			Roominfo.add(new Room(b,"Double",false,0,0,null));
+		}
+		for(int i=0;i<c;i++){
+			Roominfo.add(new Room(c,"Family",false,0,0,null));
+		}
+	}
+	private void RoomnumAdown(String RoomType){
+		for(int i=0;i<Roominfo.size();i++){
+			if(Roominfo.get(i).Type_of_room.equals(RoomType)){
+				Roominfo.get(i).No_of_room--;
+			}
+		}
+	}
+	private String checkroom(String Roomtype){
+		int rent=0;
+		int Available=0;
+		for(int i=0;i<Roominfo.size();i++){
+			if(Roominfo.get(i).Type_of_room.equals(Roomtype)){
+				Available=Roominfo.get(i).No_of_room;
+				if(Roominfo.get(i).Rent_of_room){
+					rent++;
+				}
+			}
+		}
+		//Available=Available-rent;
+		return "Available "+Available+" Rent: "+rent;
+
+	}
+	private void RoomnumAup(String RoomType){
+		for(int i=0;i<Roominfo.size();i++){
+			if(Roominfo.get(i).Type_of_room==RoomType){
+				Roominfo.get(i).No_of_room++;
+			}
+		}
+	}
+
+	public String reserveRoom(int GuestID, String hotel, String RoomType,
+			int checkindate, int checkoutdate) {
+		for(int i=0;i<Roominfo.size();i++){
+			if(Roominfo.get(i).Type_of_room.equals(RoomType)){
+				if(!Roominfo.get(i).Rent_of_room){
+					Roominfo.get(i).Guests=new Guest(GuestID,Roominfo.get(i));
+					Roominfo.get(i).Checkindate=checkindate;
+					Roominfo.get(i).Checkoutdate=checkoutdate;
+					Roominfo.get(i).Rent_of_room=true;
+					RoomnumAdown(RoomType);
+					reservationinfo.put(GuestID, Roominfo.get(i));
+					ReservavtionID++;
+					reservationcheck.put(ReservavtionID, Roominfo.get(i).Guests);
+					System.out.println();
+					break;
+				}
+			}
+		}
+		return "ResertionRoom Successful!GuesrID: "+GuestID+ "With ReservavtionID: "+ReservavtionID;
+
+	}
+
+	public String cancelRoom(int GuestID, String hotel, String RoomType,
+			int checkindate, int checkoutdate) {
+		if(reservationinfo.containsKey(GuestID)){
+			for(int i=0;i<Roominfo.size();i++){
+				if(Roominfo.get(i).Type_of_room.equals(RoomType)&&Roominfo.get(i).Checkindate==checkindate&&
+						Roominfo.get(i).Checkoutdate==checkoutdate){
+
+					Roominfo.get(i).Rent_of_room=false;
+					Roominfo.get(i).Guests=null;
+					Roominfo.get(i).Checkindate=0;
+					Roominfo.get(i).Checkoutdate=0;
+					RoomnumAup(RoomType);
+					reservationinfo.remove(GuestID);
+		
+				}
+			}
+		}
+		return "Cancel room successful! GuestID: "+GuestID+"with Hotel: "+hotel+" Type of Room: "+RoomType;
+
+	}
+
+	public String checkAvailability(int GuestID, String Preferredhotel,
+			String RoomType, int checkindate, int checkoutdate) {
+		hroominfo+=checkroom(RoomType)+"\n";
+		return hroominfo;
+	}
+
+	public String serviceReport(String hotel, int ServiceDate) {
+		serviceReport="";
+		for(int i=0;i<Roominfo.size();i++){
+			if(Roominfo.get(i).Checkoutdate==ServiceDate){
+				serviceReport +=i+1+"RoomType: "+Roominfo.get(i).Type_of_room+","+"Checkindate: "+Roominfo.get(i).Checkindate+"Checkoutdate: "+Roominfo.get(i).Checkoutdate+"\n";
+				System.out.println(serviceReport);
+			}
+		}
+
+		return serviceReport;
+	}
+
+	public String printSatus(String hotel, int Date) {
+		printstaue="";
+		for(int i=0;i<Roominfo.size();i++){
+			printstaue+=i+1+". "+"RoomType: "+Roominfo.get(i).Type_of_room+"Rent: "+Roominfo.get(i).Rent_of_room;
+			if(Roominfo.get(i).Guests!=null){
+			printstaue+="Checkindate: "+Roominfo.get(i).Checkindate+"Checkoutdate: "+Roominfo.get(i).Checkoutdate+"Rent GuestID: "+Roominfo.get(i).Guests.toString()+"\n";
+			}else{
+			printstaue+="Not Rent\n";
+			}
+		}
+		return printstaue;
+		
+	}
+
+	public String transferReservation(int GuestID, int ReservationID,
+			String CurrentHotel, String OtherHotel) {
+		if(reservationcheck.containsKey(ReservationID)){
+			for(int i=0;i<Roominfo.size();i++){
+				if(Roominfo.get(i).Guests.Guestid==GuestID){
+					transferinfo+=Roominfo.get(i).Guests.Guestid+",";
+					transferinfo+=OtherHotel+",";
+					transferinfo+=Roominfo.get(i).Type_of_room+",";
+					transferinfo+=Roominfo.get(i).Checkindate+",";
+					transferinfo+=Roominfo.get(i).Checkoutdate;
+					Roominfo.get(i).Rent_of_room=false;
+					Roominfo.get(i).Guests=null;
+					Roominfo.get(i).Checkindate=0;
+					Roominfo.get(i).Checkoutdate=0;
+					RoomnumAup(Roominfo.get(i).Type_of_room);
+					reservationinfo.remove(GuestID);
+//					cancelRoom(GuestID,CurrentHotel,Roominfo.get(i).Type_of_room,Roominfo.get(i).Checkindate,
+//							Roominfo.get(i).Checkoutdate);
+					break;
+				}
+			}
+		}
+         return transferinfo;
+	}
+
+
+}
